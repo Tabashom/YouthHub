8cd0c6a5d0a1c9eb3ac4867f308212d0040c3c93
Tabashom <tabashom15-5701@diu.edu.bd>
2025-08-19 00:04:01 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
-// js/index.js
 import { auth } from './firebase.js';
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-// Check if user is logged in
 onAuthStateChanged(auth, user => {
   if (user) {
     document.getElementById('user-info').textContent = `Logged in as: ${user.email}`;
   } else {
-    window.location.href = "login.html"; // Redirect to login if not logged in
+    window.location.href = "login.html"; 
   }
 });
 
